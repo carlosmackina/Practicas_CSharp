eeb5d555cda069c9591d56afa0b102e2d94a3634
carlosmackina <133821515+carlosmackina@users.noreply.github.com>
2023-07-13 13:29:21 -0300

    Add files via upload

---
+++ HTML_Winforms/HTML_Winforms/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTML_Winforms
+{
+     
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect=false, ValidateNames=true, Filter= "HTML|*.html" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = ofd.FileName;
+                    FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    webBrowser1.DocumentStream = fs;
+                }
+            }
+        }
+    }
+}
